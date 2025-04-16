2b2727a26dce926a54f24ae0b5410f881aff2590
KilJae69 <adi.toromanovic@outlook.com>
2025-04-16 15:42:30 +0200

    .

--- components/GalleryContent.tsx
+++ components/GalleryContent.tsx
@@ -1,32 +1,16 @@
-// import { sanityFetch } from "@/sanity/lib/live";
+import { sanityFetch } from "@/sanity/lib/live";
 import { LATEST_IMAGES } from "@/sanity/lib/queries";
-
+import GallerySwiper from "./GallerySwiper";
 import PrimaryButton from "./shared/PrimaryButton";
-import { client } from "@/sanity/lib/client";
-import dynamic from "next/dynamic";
 
-// Simple skeleton loader
-function GallerySkeleton() {
-  return (
-    <div className="mt-12 lg:mt-24 flex gap-6">
-      {[...Array(4)].map((_, i) => (
-        <div key={i} className="h-[350px] w-full rounded-xl bg-gray-100 animate-pulse" />
-      ))}
-    </div>
-  );
-}
-
-const DynamicGallerySwiper = dynamic(() => import('../components/GallerySwiper'),{
-  loading: () => <GallerySkeleton/>
-})
 
 
 export default async function GalleryContent() {
-  const images = await client.fetch(LATEST_IMAGES);
-console.log(images);
+  const { data: { images } } = await sanityFetch({ query: LATEST_IMAGES });
+
   return (
     <>
-      {images && <DynamicGallerySwiper images={images.images}/>}
+      <GallerySwiper images={images} />
       <div className="w-full flex items-center justify-center mt-6">
         <PrimaryButton href="/gallery">View More</PrimaryButton>
       </div>
