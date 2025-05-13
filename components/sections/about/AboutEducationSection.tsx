5aef6c9365652a993bca22843b893d0fc7ff01a7
KilJae69 <adi.toromanovic@outlook.com>
2025-05-13 16:42:52 +0200

    .

--- components/sections/about/AboutEducationSection.tsx
+++ components/sections/about/AboutEducationSection.tsx
@@ -4,35 +4,35 @@ import SectionH2Title from "@/components/shared/SectionH2Title";
 import { GridBackground } from "@/components/ui/grid-background";
 import { educationData } from "@/constants/educationData";
 import { cn } from "@/lib/utils";
-import Image from "next/image";
+// import Image from "next/image";
 
-interface CredentialCardProps {
-  text: string;
-  borderColor: string;
-}
+// interface CredentialCardProps {
+//   text: string;
+//   borderColor: string;
+// }
 
-function CredentialCard({ text, borderColor }: CredentialCardProps) {
-  return (
-    <div
-      className="p-6 md:p-8 rounded-[32px] bg-[#fdfdfd] h-full shadow-sm max-w-[400px] md:max-w-[450px]"
-      style={{
-        border: `2px solid ${borderColor}`,
-        boxShadow: `-4px 0px 0px 0px ${borderColor}`,
-      }}
-    >
-      <p className="text-text-dark text-lg md:text-xl font-medium text-center">
-        {text}
-      </p>
-    </div>
-  );
-}
+// function CredentialCard({ text, borderColor }: CredentialCardProps) {
+//   return (
+//     <div
+//       className="p-6 md:p-8 rounded-[32px] bg-[#fdfdfd] h-full shadow-sm max-w-[400px] md:max-w-[450px]"
+//       style={{
+//         border: `2px solid ${borderColor}`,
+//         boxShadow: `-4px 0px 0px 0px ${borderColor}`,
+//       }}
+//     >
+//       <p className="text-text-dark text-lg md:text-xl font-medium text-center">
+//         {text}
+//       </p>
+//     </div>
+//   );
+// }
 
 export default function AboutEducationSection() {
   return (
     <section className="overflow-hidden relative py-12 pb-8  lg:py-24  bg-light-blue-bg">
       <GridBackground />
       <Container className="relative">
-        <Image
+        {/* <Image
           className=" absolute bottom-20 -left-32"
           src="/assets/green-flower-shape.svg"
           width={120}
@@ -45,26 +45,30 @@ export default function AboutEducationSection() {
           width={120}
           height={120}
           alt="flower svg shape"
-        />
+        /> */}
         <FadeIn className="text-center">
           <SectionH2Title text="My" accentText="Education" />
         </FadeIn>
 
-        <FadeInStagger className="relative mt-12 lg:mt-24 w-full grid md:grid-cols-2 justify-center mx-auto gap-5   max-w-5xl ">
-          {educationData.map((education, index) => (
-            <FadeIn
-              direction={
-                index % 2 === 0 ? "right" : "left" // Alternate between right and left
-              }
-              key={education.id}
-              className={cn(`relative `, )}
-            >
-              <CredentialCard
+        <FadeInStagger className="relative mt-12 lg:mt-24 w-full  justify-center mx-auto    max-w-5xl ">
+          <ul className="grid md:grid-cols-2 gap-10">
+            {educationData.map((education, index) => (
+              <FadeIn
+                direction={
+                  index % 2 === 0 ? "right" : "left" // Alternate between right and left
+                }
+                key={education.id}
+                className={cn(`relative `)}
+              >
+                <li className="flex items-start gap-4">
+                  <span className="block size-10 shrink-0 rounded-full" style={{backgroundColor:education.color}}/>
+                  <p className="font-semibold text-xl lg:text-2xl">{education.text}</p>
+                </li>
+                {/* <CredentialCard
                 text={education.text}
-                borderColor={education.color}
-                
-              />
-              {index === 1 && (
+                borderColor={education.color}              
+              /> */}
+                {/* {index === 1 && (
                 <Image
                   className="absolute -top-[2.6em] -right-[0.6em] rotate-6"
                   style={{
@@ -76,9 +80,10 @@ export default function AboutEducationSection() {
                   width={60}
                   height={60}
                 />
-              )}
-            </FadeIn>
-          ))}
+              )} */}
+              </FadeIn>
+            ))}
+          </ul>
           {/* Master of Arts Card */}
 
           {/* <div className="absolute left-[5%] top-[15%] transform -rotate-6">
--- constants/educationData.ts
+++ constants/educationData.ts
@@ -3,24 +3,24 @@ export const educationData = [
     id: 1,
     text: "Master of Arts in Clinical Psychology from the University of Houston-Clear Lake",
     rotation:"-rotate-3 md:-rotate-6 ",
-    color: "#a5da84",
+    color: "#66816b",
   },
   {
     id: 2,
     text: "Registered Play Therapist (RPT)",
     rotation:"rotate-3 md:rotate-6 md:translate-y-30",
-    color: "#ff9b3e",
+    color: "#dc8b51",
   },
   {
     id: 3,
     text: "Licensed Mental Health Counselor (LMHC)",
     rotation:"-rotate-3 md:-rotate-6 md:-translate-y-20",
-    color: "#ffde76",
+    color: "#d17d62",
   },
   {
     id: 4,
     text: "Certification in Eye Movement Desensitization and Reprocessing (EMDR) (Persuing)",
     rotation:"rotate-3 md:rotate-6 md:translate-y-10",
-    color: "#ffbcd6",
+    color: "#65826c",
   },
 ] as const;
