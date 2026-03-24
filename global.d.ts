18363c8754e1637b2cf6a5da4d7427447fe48ac5
Veladzic S <veladzicsenahid@gmail.com>
2026-03-24 17:41:04 +0100

    build fix v2

--- global.d.ts
+++ global.d.ts
@@ -2,3 +2,7 @@ declare module "*.css" {
   const content: Record<string, string>;
   export default content;
 }
+
+declare module "swiper/css/bundle";
+declare module "swiper/css/free-mode";
+declare module "swiper/css/*";
